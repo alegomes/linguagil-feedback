d5a0a83c681ef67fd5d348b6f54f252953d77400
Alexandre Gomes <alegomes@gmail.com>
2010-09-20 12:08:04 -0300

    inclusao do REDAME

---
+++ README.markdown
@@ -0,0 +1,1 @@
+@serge_rehem told me he'd like to use Twitter as the official channel for 'feedbacking' LinguAgil conference. Well, then I drafted this. 
--- feedback.rb
+++ feedback.rb
@@ -8,6 +8,8 @@ get '/' do
   @good_tweets = Twitter::Search.new('linguagil foibom -podemelhorar')
   @bad_tweets = Twitter::Search.new('linguagil podemelhorar -foibom')
   @suggestion_tweets = Twitter::Search.new('linguagil sugestao -foibom -podemelhorar')
+  @other_tweets = Twitter::Search.new('linguagil -sugestao -foibom -podemelhorar')
+
     
   erb :index
 end
