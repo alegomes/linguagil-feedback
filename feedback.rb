67a5234d0f23be7955f054ee8c5bfd5d0ee7b7db
Alexandre Gomes <alegomes@gmail.com>
2010-09-20 09:31:24 -0300

    comecando a listar tweets

--- feedback.rb
+++ feedback.rb
@@ -1,6 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'twitter'
+require 'erb'
 
 get '/' do
-  "Hello World!"
+  @tweets = []
+  
+  Twitter::Search.new('linguagil').each do |r| 
+    # puts ">> #{r.from_user} => #{r.text}"
+    @tweets << r
+  end
+  
+  erb :index
 end
